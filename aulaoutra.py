5f6fcd28096f1c124034c66b2678142972cf2977
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-02-12 10:51:21 -0300

    Create aulaoutra.py

---
+++ aulaoutra.py
@@ -0,0 +1,19 @@
+#variavel é o nome que você da a um valor ou alguma coisa especifica evita ter que precisar editar. este é um comentário.
+
+faturamento = 1000
+custo = 700 
+lucro = 1000 - 700
+
+print("o faturamento foi de", 1000)
+print("o custo foi de", 700)
+print("o lucro foi de", 1000-700)
+
+#as aspas só servem para texto
+
+#int= números inteiros
+
+#variavel booleano descobre true or false
+
+#números com casa decimal é um FLOAT (números quebrados)
+
+teve_lucro true 
