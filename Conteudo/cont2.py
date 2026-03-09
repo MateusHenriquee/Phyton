a8d7bbb3cde343f572dcd5e47b87ef9ec2bef1c9
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-03-09 11:44:28 -0300

    Update cont2.py

--- Conteudo/cont2.py
+++ Conteudo/cont2.py
@@ -22,3 +22,10 @@ print(100==100)
 #2 lugar a ser calculado:
 #3 lucar a ser calculado: multiplicação e divisao
 #4 lugar a ser calculado: soma e subtração
+
+#exemplos
+
+print(5+3*2)
+print(3*5+4**2)
+print(3*(5+4)**2)
+print()
