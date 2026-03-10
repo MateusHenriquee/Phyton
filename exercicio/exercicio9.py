09f06ea6da96db0a21e30a520d6c0d9da9681b7c
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-03-10 08:59:22 -0300

    SIM

--- exercicio/exercicio9.py
+++ exercicio/exercicio9.py
@@ -1,6 +1,6 @@
 nome= input("nome do aluno:")
-nota1= int(input("valor da sua primeira nota:"))
-nota2= int(input("valor da sua segunda nota:"))
+nota1= float(input("valor da sua primeira nota:"))                  #float pq o numero da nota pode ser numero com virgula tlgd
+nota2= float(input("valor da sua segunda nota:"))
 soma= nota1 + nota2
 
-print("nome do aluno: {} sua primeira nota foi {} e sua segunda nota foi {} e a sua média é: {}" .format(nome, nota1, nota2, soma/2))
+print("nome do aluno: {} sua primeira nota foi {} e sua segunda nota foi {} e a sua média é: {}" .format(nome, nota1, nota2, (soma)/2))
