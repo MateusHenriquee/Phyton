863bca231b9dad6068830a28acb99c0433572289
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-03-16 12:00:52 -0300

    coisa

---
+++ exercicio/exercicio12.py
@@ -0,0 +1,4 @@
+valorcartera = float(input("quantos reais vc tem na carteira? : "))
+dolar= (5.38)
+converter = valorcartera / dolar
+print("vc tem na carteira {:.2f} reais e esse valor convertido em dolár é {:.2f} dolares.".format(valorcartera,converter))
---
+++ exercicio/exercicio13.py
@@ -0,0 +1,4 @@
+altura= float(input("altura de uma parede: "))
+largura = float(input("largura de uma parede: "))
+tinta= altura * largura / 2
+print("a area de uma parede é sua largura: {} multiplicado pela sua largura: {}, cada litro de tinta pinta uma area de 2 metros quadrados, e a quantidade de tinta necessária é {}. ".float(largura,altura,tinta))
