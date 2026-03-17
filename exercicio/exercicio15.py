fe045e42b84333ba7c105b4c1da58a48e199e528
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-03-17 12:27:48 -0300

    cu

--- exercicio/exercicio15.py
+++ exercicio/exercicio15.py
@@ -1,4 +1,4 @@
 salario= float(input("salario atual: "))
 clt= float(input("salário com aumento de:"))
 promocao= salario + ( salario  * clt / 100)
-print("o produto que custa {:.2f} com aumento de {:.0f} porcento fica {:.2f} ".format(salario,clt,promocao))
+print("o o salario de {:.2f} com aumento de {:.0f} porcento fica {:.2f} ".format(salario,clt,promocao))
