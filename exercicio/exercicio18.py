7a2d8509f803000ab3d399530b532c80ad9a0757
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-03-23 12:23:24 -0300

    pensando normal

--- exercicio/exercicio18.py
+++ exercicio/exercicio18.py
@@ -1,4 +1,17 @@
-import math
-nmr = int(input("digite um numero: "))
-nmrl= math.sqrt(nmr)
-print("a raiz de {} é igual a {}".format(nmr,nmrl))
+#import math
+#nmr = int(input("digite um numero: "))
+#nmrl= math.sqrt(nmr)
+#print("a raiz de {} é igual a {}").format(nmr, math.ceil(nmrl))
+
+#import math
+#nmr = int(input("digite um numero: "))
+#nmrl= math.sqrt(nmr)
+#print("a raiz de {} é igual a {}").format(nmr, math.ceil(nmrl))
+
+#importando funcionalidades específicas
+
+from math import sqrt,floor,ceil
+
+nmr = int(input("digite seu numero: "))
+nmrl = sqrt(nmr)
+print("")
