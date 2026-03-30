eac7bb516a44b3b215f13c701c49deb3e6a685b5
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-03-30 08:11:09 -0300

    pensano

--- Conteudo/cont4.py
+++ Conteudo/cont4.py
@@ -1,1 +1,5 @@
 import random
+nmr= random.random()
+nmrl= random.randint(1,10)
+print(nmr)
+print(nmrl)
---
+++ exercicio/exercicio19.py
@@ -0,0 +1,3 @@
+import math
+nmr = float(input("Digite um número"))
+print("o valor digitado foi {} e a sua fração inteira é {}".format(nmr,math.trunc(nmr)))
