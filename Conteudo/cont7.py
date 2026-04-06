e9bf3c22a9fa2a81e3a1d9b4307874996fca4d2f
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-04-06 08:58:34 -0300

    pensando mt msm

---
+++ Conteudo/cont7.py
@@ -0,0 +1,13 @@
+fr= "cursodeanaliseededesenvolvimentodesistemas"    
+print(fr[3])                               #isso mostra o espaço da memoria
+print(fr[3:40])                            #intervalo de caracteres menos o ultimo caractere
+print(fr[3:2])                             #pula de 2 em 2
+print(fr[:19])                             #do caractere q escolheu até o final
+print(fr[1::19])                           #do caractere q escolheu vai pulando do numero q escolheu ate o final
+
+print(len(fr))                             #conta quantitade de caracteres
+print(fr.count("e"))                       #procura o caractere especifico na variavel
+print(fr.count("e", 1,19))                 #procura o caractere num intervalo específico
+print(fr.find("desenvolvimento"))          #ver onde começa
+print(fr.find("android"))                  #-1 pq n tem issae
+print("sistemas"in(fr))   
