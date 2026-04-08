d5191bc3f0fe18fd877f9a1e2494015da1a9d239
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-04-08 11:49:50 -0300

    Update cont7.py

--- Conteudo/cont7.py
+++ Conteudo/cont7.py
@@ -1,4 +1,4 @@
-fr= "cursodeanaliseededesenvolvimentodesistemas"    
+fr= "CursoDeanaliseedeDesenvolvimentodeSistemas"    
 print(fr[3])                               #isso mostra o espaço da memoria
 print(fr[3:40])                            #intervalo de caracteres menos o ultimo caractere
 print(fr[3:2])                             #pula de 2 em 2
@@ -11,3 +11,14 @@ print(fr.count("e", 1,19))                 #procura o caractere num intervalo es
 print(fr.find("desenvolvimento"))          #ver onde começa
 print(fr.find("android"))                  #-1 pq n tem issae
 print("sistemas"in(fr))                    #verifica se aquele conjunto string ta presente na variavel
+
+print(fr.replace("sistemas","seloko"))     #troca a palavra 
+print(fr.upper())                          #deixa maiusculo
+print(fr.lower())                          #minusculo
+print(fr.capitalize())                     #primeira letra da primmeira palavra em maisculo
+print(fr.title())                          #primeira letra de cada primmeira palavra em maisculo
+print(fr.strip())                          #tira os espaços em branco
+print(fr.rstrip())                         #direita
+print(fr.lstrip())                         #esquerda
+print(fr.split())
+print("&".join(fr))                        #une um simbolo em cada parte do split
