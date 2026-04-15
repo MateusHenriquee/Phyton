e959c8697917036f01be5c3bf2b3d1426ec1fdba
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-04-15 09:18:00 -0300

    Create aaaaaaaaaa.py

---
+++ Atividade/aaaaaaaaaa.py
@@ -0,0 +1,15 @@
+tx = "   Olá Mundo!   "
+txsla = tx.strip()
+print("Ambos os lados: '{}'" .format(txsla))
+
+tx_esquerda = tx.lstrip()
+print("Apenas esquerda: '{}'" .format(tx_esquerda))
+
+tx_direita = tx.rstrip()
+print("Somente direita: '{}'" .format(tx_direita))
+
+
+
+slamn= "uma palavra que nao temaqui"
+
+print(slamn.find("pensando"))
