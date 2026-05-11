e3f5ecdae24a11d11c04f6d0c3c90d8e34ed341e
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-05-11 09:01:42 -0300

    pensando

---
+++ exercicio/exercício40.py
@@ -0,0 +1,11 @@
+nmr= int(input("digite um numero: "))
+nmrl= int(input("digite outro numero: "))
+
+if nmr > nmrl:
+    print("o primeiro valor é maior: {}".format(nmr))
+
+elif nmr < nmrl:
+    print("o segundo valor é maior: {}".format(nmrl))
+    
+else:
+    print("os valores sao")
