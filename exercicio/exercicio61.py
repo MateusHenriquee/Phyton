c1e3564f7ba2cfcff07315284f8a75965c7a8e1e
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-06-30 08:57:28 -0300

    complicado

---
+++ exercicio/exercicio61.py
@@ -0,0 +1,36 @@
+n1 = float(input('Primeiro valor: '))
+n2 = float(input('Segundo valor: '))
+opcao = 0
+
+while opcao != 5:
+    print('''    --- MENU ---
+    [1] Somar
+    [2] Multiplicar
+    [3] Maior
+    [4] Novos números
+    [5] Sair do programa''')
+    opcao = int(input('>>>> Qual é a sua opção? '))
+
+    if opcao == 1:
+        soma = n1 + n2
+        print(f'A soma entre {n1} e {n2} é {soma}.')
+    elif opcao == 2:
+        produto = n1 * n2
+        print(f'O resultado de {n1} x {n2} é {produto}.')
+    elif opcao == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print(f'Entre {n1} e {n2} o maior valor é {maior}.')
+    elif opcao == 4:
+        print('Informe os números novamente:')
+        n1 = float(input('Primeiro valor: '))
+        n2 = float(input('Segundo valor: '))
+    elif opcao == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente.')
+    print('=-=' * 10)
+
+print('Fim do programa! Volte sempre.')
