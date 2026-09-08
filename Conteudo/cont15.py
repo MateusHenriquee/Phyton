7abf868d164d276f363040ebf9a8fa02206e813b
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-09-08 09:16:33 -0300

    Update cont15.py

--- Conteudo/cont15.py
+++ Conteudo/cont15.py
@@ -3,14 +3,39 @@
 
 # Para mostrar-mos um item dentro de uma lista interna primeiro colocamos o índice da lista depois o índice do item dentro da lista. Ex print(pessoas[0][0]) irá mostrar 'pedro', print (pessoas[1][1] vai aparecer 19, print(pessoas[2][0]) vai aparecer joão, print(pessoas[1]) vai aparecer ['maria',19]
 
-teste = []
-teste.append("sla")
-teste.append(1)
-galera = []
+# exemplo 1
+
+# teste = []
+# teste.append("sla")
+# teste.append(1)
+# galera = []
 # galera.append(teste)
-galera.append(teste[:])
-teste [0] = "m"
-teste [1] = 0
-galera.append(teste[:])
-print(teste)
+# galera.append(teste[:])
+# teste [0] = "m"
+# teste [1] = 0
+# galera.append(teste[:])
+# print(teste)
+# print(galera)
+
+# exemplo 2
+
+# galera = [['sla', 19], ["nsei", 1],["nao", 0],["sim" ,10]]
+
+# print(galera[2][1])
+
+# for p in galera:
+#     print(f'{p[0]} tem {p[1]} anos de idade')
+
+galera = []
+
+dado = []
+
+que = q = 0
+
+for c in range (0,3):
+    dado.append(str(input("nome: ")))
+    dado.append(int(input("idade: ")))
+    galera.append(dado)
+    dado.clear()
 print(galera)
+    
