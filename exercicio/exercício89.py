dbce03369e6aa8a7aac8a33e3413f89dcc8cf517
mateos_0 <mateus.martins.souza01@escola.pr.gov.br>
2026-09-22 11:53:18 -0300

    ahhh

---
+++ exercicio/exercício89.py
@@ -0,0 +1,33 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+soma_pares = 0
+soma_col3 = 0
+maior_lin2 = 0
+
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f"Digite um valor para [{linha}, {coluna}]: "))
+
+print("-" * 30)
+
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f"[{matriz[linha][coluna]:^5}]", end="")
+     
+        if matriz[linha][coluna] % 2 == 0:
+            soma_pares += matriz[linha][coluna]
+            
+    print() 
+print("-" * 30)
+
+for linha in range(0, 3):
+    soma_col3 += matriz[linha][2]
+
+for coluna in range(0, 3):
+    if coluna == 0:
+        maior_lin2 = matriz[1][coluna]
+    elif matriz[1][coluna] > maior_lin2:
+        maior_lin2 = matriz[1][coluna]
+
+print(f"A) A soma dos valores pares é: {soma_pares}")
+print(f"B) A soma dos valores da terceira coluna é: {soma_col3}")
+print(f"C) O maior valor da segunda linha é: {maior_lin2}")
